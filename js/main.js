a021837e496220e35ab70f7fe93ade3adcedfdb1
Nikolai5 <nikolaj.janko@gmail.com>
2015-11-09 19:00:15 +0100

    Cleaning textures

--- js/main.js
+++ js/main.js
@@ -5,6 +5,76 @@ window.onload = function(){
 	// Load the BABYLON 3D engine
 	var engine = new BABYLON.Engine(canvas, true);
 
+    function randomNumber(min, max){
+        return (Math.random()*(max-min))+min;
+    }
+
+    Comet = function(radius, scene) {
+        // Call the super class BABYLON.Mesh
+        BABYLON.Mesh.call(this, "comet", scene);
+
+
+        // Sphere shape creation
+        var vertexData = BABYLON.VertexData.CreateSphere(radius,2);
+        // Apply the shape to our tree
+        vertexData.applyToMesh(this, false);
+
+        var positions = this.getVerticesData(BABYLON.VertexBuffer.PositionKind);
+        var numberOfPoints = positions.length/3;
+
+        // Build a map containing all vertices at the same position
+        var map = [];
+        for (var i=0; i<numberOfPoints; i++) {
+            var p =new  BABYLON.Vector3(positions[i*3], positions[i*3+1], positions[i*3+2]);
+
+            var found = false;
+            for (var index=0; index<map.length&&!found; index++) {
+                var array = map[index];
+                var p0 = array[0];
+                if (p0.equals (p) || (p0.subtract(p)).lengthSquared() < 0.01){
+                    array.push(i*3);
+                    found = true;
+                }
+            }
+            if (!found) {
+                var array = [];
+                array.push(p, i*3);
+                map.push(array);
+            }
+        }
+/*
+        var that = this;
+        // For each vertex at a given position, move it with a random value
+        map.forEach(function(array) {
+            var index, min = 0, max = 3;
+            var rx = randomNumber(min,max);
+            var ry = randomNumber(min,max);
+            var rz = randomNumber(min,max);
+
+            for (index = 1; index<array.length; index++) {
+                var i = array[index];
+                positions[i] += rx;
+                positions[i+1] += ry;
+                positions[i+2] += rz;
+            }
+        });
+*/
+
+        this.convertToFlatShadedMesh();
+    
+    };
+
+    // Our object is a BABYLON.Mesh
+    Comet.prototype = Object.create(BABYLON.Mesh.prototype);
+    // And its constructor is the Tree function described above.
+    Comet.prototype.constructor = Comet;
+
+   
+
+    function createCometNest(){
+
+    }
+
 	function createScene() {
 		var scene = new BABYLON.Scene(engine);
 
@@ -14,42 +84,11 @@ window.onload = function(){
 		var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
 		camera.attachControl(canvas, true);
-		//camera.rotation.y=-0.5;
-		//camera.position.z=-100;
-		//camera.position.y=+100;
 
 
-/*
-        // Fog
-        scene.fogMode = BABYLON.Scene.FOGMODE_EXP;
-        //BABYLON.Scene.FOGMODE_NONE;
-        //BABYLON.Scene.FOGMODE_EXP;
-        //BABYLON.Scene.FOGMODE_EXP2;
-        //BABYLON.Scene.FOGMODE_LINEAR;
-
-        scene.fogColor = new BABYLON.Color3(0.9, 0.9, 0.85);
-        scene.fogDensity = 0.01;
-
-        //Only if LINEAR
-        //scene.fogStart = 20.0;
-
-        */
-        //scene.fogEnd = 60.0;
+        var comet = new Comet(2, scene);
 
 
-/*
-        var skyboxMaterial = new BABYLON.ShaderMaterial("skyboxMaterial", scene, {
-            vertexElement: "vertexShaderCode",
-            fragmentElement: "fragmentShaderCode",
-        },
-        {
-            attributes: ["position", "uv"],
-            uniforms: ["worldViewProjection"]
-        });
-        
-        var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-        ground.material = skyboxMaterial;
-        */
 
         // Skybox
         var skybox = BABYLON.Mesh.CreateBox("skyBox", 9000.0, scene);
@@ -57,7 +96,7 @@ window.onload = function(){
         var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
         skyboxMaterial.backFaceCulling = false;
         skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/sky1/sky1", scene);
-        //skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
         skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
         skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
         skyboxMaterial.disableLighting = true;
@@ -66,13 +105,6 @@ window.onload = function(){
 
         skybox.material = skyboxMaterial;
 
-        var alpha = 255;
-        scene.registerBeforeRender(function () {
-
-        	scene.fogDensity = Math.cos(alpha) / 10;
-            //alpha += 0.02;
-
-        });
 
     	// Create the "God Rays" effect (volumetric light scattering)
     	var godrays = new BABYLON.VolumetricLightScatteringPostProcess('godrays', 1.0, camera, null, 80, BABYLON.Texture.BILINEAR_SAMPLINGMODE, engine, false);
@@ -85,22 +117,15 @@ window.onload = function(){
     	godrays.mesh.scaling = new BABYLON.Vector3(200, 200, 200);
 
     	/*
+        // Godrays parameters
     	godrays.exposure = 0.4;
 		godrays.decay = 0.96815;
 		godrays.weight = 0.358767;
 		godrays.density = 0.9526;
-*/
+        */
 
     	light.position = godrays.mesh.position;
 
-    	/*
-    	for(var i=0;i<200;i++){
-    		var point = new BABYLON.Mesh("point", scene);
-    		point.position = new BABYLON.Vector3(0,Math.random()*100,Math.random()*100);
-    		point.material=material1
-    	}
-
-    	*/
 
     	// Create earth
     	var earthMaterial = new BABYLON.StandardMaterial("earth", scene);
@@ -111,8 +136,6 @@ window.onload = function(){
 		earthMaterial.emissiveTexture = new BABYLON.Texture("textures/earthlights2.jpg", scene);
 		earthMaterial.specularPower = 100; 
 
-		//var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(10, 50, 50), scene);
-
 
 		var earth = BABYLON.Mesh.CreateSphere("sphere", 100.0, 1500.0, scene);
 		earth.material=earthMaterial;
@@ -120,6 +143,7 @@ window.onload = function(){
 		earth.rotation.x=Math.PI;
 
 
+        // Earth rotation
 		scene.actionManager.registerAction(new BABYLON.IncrementValueAction(BABYLON.ActionManager.OnEveryFrameTrigger, earth, "rotation.y", 0.0002));
 
 		var atmosphere = BABYLON.Mesh.CreateSphere("atmosphere", 100.0, 1505.0, scene);
@@ -132,26 +156,12 @@ window.onload = function(){
 		atmosphereMaterial.specularPower = 2;
 		atmosphere.material = atmosphereMaterial;
 		atmosphere.position = earth.position;
-/*
-		atmosphereMaterial.diffuseFresnelParameters = new BABYLON.FresnelParameters();
-    	atmosphereMaterial.diffuseFresnelParameters.bias = 0.5;
-    	atmosphereMaterial.diffuseFresnelParameters.power = 2;
-    	atmosphereMaterial.diffuseFresnelParameters.rightColor = BABYLON.Color3.Black();
-    	atmosphereMaterial.diffuseFresnelParameters.leftColor = BABYLON.Color3.White();
-    	*/
+
     	atmosphereMaterial.opacityFresnelParameters = new BABYLON.FresnelParameters();
     	atmosphereMaterial.opacityFresnelParameters.bias = 0.4;
     	atmosphereMaterial.opacityFresnelParameters.power = 1;
     	atmosphereMaterial.opacityFresnelParameters.rightColor = BABYLON.Color3.Black();
     	atmosphereMaterial.opacityFresnelParameters.leftColor = BABYLON.Color3.White();
-/*
-    	atmosphereMaterial.emissiveFresnelParameters = new BABYLON.FresnelParameters();
-    	atmosphereMaterial.emissiveFresnelParameters.bias = 0.5;
-    	atmosphereMaterial.emissiveFresnelParameters.power = 2;
-    	atmosphereMaterial.emissiveFresnelParameters.rightColor = BABYLON.Color3.Black();
-    	atmosphereMaterial.emissiveFresnelParameters.leftColor = BABYLON.Color3.White();
-    	*/
-		/////
 
 		var light0 = new BABYLON.HemisphericLight("ambilight", new BABYLON.Vector3(0, 1, 0), scene);
 		light0.diffuse = new BABYLON.Color3(0.1, 0.1, 0.1);
